1f92dda432bbe69acadb851e14619d7a1ea899d4
Xiao Ling <lingxiao1002@gmail.com>
2019-01-15 17:22:00 +0800

    Added code for reading barcodes.

--- examples/react-wasm-barcode/src/App.js
+++ examples/react-wasm-barcode/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Barcode} from './Barcode';
 
 class App extends Component {
   render() {
@@ -8,17 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Barcode/>
         </header>
       </div>
     );
