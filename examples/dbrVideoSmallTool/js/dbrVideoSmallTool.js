16d7a2039baf5dbbd0d359a896d66847b1ea4e36
Keillion <735699921@qq.com>
2019-02-19 11:29:23 +0800

    readme

--- examples/dbrVideoSmallTool/js/dbrVideoSmallTool.js
+++ examples/dbrVideoSmallTool/js/dbrVideoSmallTool.js
@@ -28,20 +28,20 @@ dynamsoft.dbrEnv.bUseWorker = true;
  * | ---------------- | -------------- | -----------
  * | callback         | function(txt)  | 
  *     It is called each time a reliable result is obtained.
- *     The same neigbour results would be regarded as one result.
+ *     The same neighbour results would be regarded as one result.
  * | ---------------- | -------------- | -----------
  * | count            | number         | 
  *     if `count` is 0, the window would not automatically close. Default 0.
- *     if `count` == 1, close window after get a relibale result.
- *     if the `count` > 1, the window will close automatically after `count` reliable results is obtained.
- *     The same neigbour results would be regarded as one result.
+ *     if `count` == 1, the application will close the window after getting a reliable result.
+ *     if the `count` > 1, the application will close the window after `count` reliable results are obtained.
+ *     The same neighbour results would be regarded as one result.
  * | ---------------- | -------------- | -----------
  * | confidence       | number         | 
- *     A raw result, whose confidence equal or large than the confidence, will be regarded as a reliable result. Dafault 30.
+ *     A raw result, whose confidence equals or larger than the confidence, will be regarded as a reliable result. Dafault 30.
  * | ---------------- | -------------- | -----------
  * | styleObj         | object         | 
  *     A key-value style object to modify the style of `.dbrVideoSmallTool-outer`. 
- *     It is usefull when you need to set the position or the size of the tool.
+ *     It is useful when you need to set the position or the size of the tool.
  * 
  *     e.g. `{postion: 'absolute', margin: '0', left: '200px', top: '100px', width: '200px', height: '160px'}`
  * 
