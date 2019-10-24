fee8d8a9803b1ce94552e4801a83877461c28004
Keillion <735699921@qq.com>
2019-10-24 10:40:12 +0800

    7.1.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dynamsoft:javascript-barcode',
-  version: '7.1.2',
+  version: '7.1.3',
   summary: 'A JavaScript barcode library that enables you to build web barcode reader app',
   git: 'https://github.com/dynamsoft-dbr/javascript-barcode.git',
   documentation: 'README.md'
@@ -13,14 +13,14 @@ Package.onUse(function(api) {
     'dist/dbr.esm.d.ts',
     'dist/dbr.esm.min.js',
     'dist/dbr.min.js',
-    'dist/dbr-7.1.2.d.ts',
-    'dist/dbr-7.1.2.esm.d.ts',
-    'dist/dbr-7.1.2.esm.min.js',
-    'dist/dbr-7.1.2.min.js',
-    'dist/dbr-7.1.2.wasm',
-    'dist/dbr-7.1.2.wasm.min.js',
-    'dist/dbr-7.1.2.wasm.withio.min.js',
-    'dist/dbr-7.1.2.withio.wasm'
+    'dist/dbr-7.1.3.d.ts',
+    'dist/dbr-7.1.3.esm.d.ts',
+    'dist/dbr-7.1.3.esm.min.js',
+    'dist/dbr-7.1.3.min.js',
+    'dist/dbr-7.1.3.wasm',
+    'dist/dbr-7.1.3.wasm.min.js',
+    'dist/dbr-7.1.3.wasm.withio.min.js',
+    'dist/dbr-7.1.3.withio.wasm'
   ], 'client');
 });
 
