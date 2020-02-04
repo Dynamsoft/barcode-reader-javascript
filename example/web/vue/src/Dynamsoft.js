581d29da584e763dcb4d13ef27fc3306933dc62c
Keillion <735699921@qq.com>
2020-02-04 10:35:06 +0800

    ios12

--- example/web/vue/src/Dynamsoft.js
+++ example/web/vue/src/Dynamsoft.js
@@ -1,5 +1,5 @@
 import Dynamsoft from "keillion-dynamsoft-javascript-barcode";
-Dynamsoft.BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/keillion-dynamsoft-javascript-barcode@0.20200204.0/dist/";
+Dynamsoft.BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/keillion-dynamsoft-javascript-barcode@0.20200204.1/dist/";
 // Please visit https://www.dynamsoft.com/CustomerPortal/Portal/TrialLicense.aspx to get a trial license
 Dynamsoft.BarcodeReader.productKeys = "PRODUCT-KEYS";
 // Dynamsoft.BarcodeReader._bUseFullFeature = true; // Control of loading min wasm or full wasm.
