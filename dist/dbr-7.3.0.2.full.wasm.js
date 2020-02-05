49a9e8a7e7cb8ebd29fa864db38e5186a6de3b40
Keillion <735699921@qq.com>
2020-02-05 17:35:37 +0800

    ios12

--- dist/dbr-7.3.0.2.full.wasm.js
+++ dist/dbr-7.3.0.2.full.wasm.js
@@ -256,7 +256,7 @@ function La() {
   e.HEAPF32 = Ja = new Float32Array(buffer);
   e.HEAPF64 = Ka = new Float64Array(buffer);
 }
-var qa = 1540752;
+var qa = 1540704;
 assert(!0, "stack must start aligned");
 assert(!0, "heap must start aligned");
 e.TOTAL_STACK && assert(5242880 === e.TOTAL_STACK, "the stack size can no longer be determined at runtime");
@@ -271,9 +271,9 @@ wa && (buffer = wa.buffer);
 Ma = buffer.byteLength;
 assert(0 === Ma % 65536);
 La();
-p[qa >> 2] = 6783648;
+p[qa >> 2] = 6783600;
 function Na() {
-  var a = y[385193], b = y[385194];
+  var a = y[385181], b = y[385182];
   34821223 == a && 2310721022 == b || m("Stack overflow! Stack cookie has been overwritten, expected hex dwords 0x89BACDFE and 0x02135467, but received 0x" + b.toString(16) + " " + a.toString(16));
   1668509029 !== p[0] && m("Runtime error: The application has corrupted its heap memory area (address zero)!");
 }
@@ -2434,7 +2434,7 @@ function ae(a) {
   ae.ja = c;
   return ae.ja;
 }
-Da("GMT", 1540656, 4);
+Da("GMT", 1540608, 4);
 function be() {
   function a(a) {
     return (a = a.toTimeString().match(/\(([A-Za-z ]+)\)$/)) ? a[1] : "GMT";
@@ -3420,7 +3420,7 @@ var Z = e.asm({}, {ClassHandle:Nc, ClassHandle_clone:Vc, ClassHandle_delete:Wc, 
   return eval(x(a)) | 0;
 }, emscripten_run_script_string:Y, ensureOverloadTable:dd, exposePublicSymbol:ed, extendError:Ic, fabs:Xa, fabsf:Xa, floatReadValueFromPointer:Qd, floor:$a, flushPendingDeletes:$c, genericPointerToWireType:jd, getBasestPointer:wd, getInheritedInstance:xd, getInheritedInstanceCount:rd, getLiveInheritedInstances:td, getShiftFromSize:Ac, getTypeName:Dd, get_first_emval:Od, getenv:ae, heap32VectorToArray:Jd, init_ClassHandle:bd, init_RegisteredPointer:Ad, init_embind:vd, init_emval:Pd, integerReadValueFromPointer:Rd, 
 jsStackTrace:qb, localtime:function(a) {
-  return fe(a, 1540608);
+  return fe(a, 1540560);
 }, localtime_r:fe, makeClassHandle:yd, makeLegalFunctionName:Gc, memcpy:function(a, b, c) {
   a |= 0;
   b |= 0;
@@ -4424,8 +4424,8 @@ function wf() {
   }
   if (!(0 < bb)) {
     assert(!0);
-    y[385193] = 34821223;
-    y[385194] = 2310721022;
+    y[385181] = 34821223;
+    y[385182] = 2310721022;
     if (e.preRun) {
       for ("function" == typeof e.preRun && (e.preRun = [e.preRun]); e.preRun.length;) {
         Ua();
