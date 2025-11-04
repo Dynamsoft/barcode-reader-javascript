284b641fba57f5cf37786956f432109dc34269cf
DMGithubPublisher <github@dynamsoft.com>
2025-11-04 14:46:23 +0800

    11.2.2000

---
+++ updateAllDevDeps.js
@@ -0,0 +1,17 @@
+//Please run `updateAllDevDeps.js` in `DCV_BUNDLE` first, and then execute this script.
+
+import fs from "fs";
+import { execSync } from "child_process";
+
+const modulesSet = ["CORE", "CVR", "LICENSE", "UTILITY", "DBR", "DCP", "DCE"];
+
+for (let module of modulesSet) {
+  const pkg = JSON.parse(fs.readFileSync(`../${module}/package.json`, "utf-8"));
+  if(module === "CORE") {
+    execSync(`npm pkg set devDependencies.dynamsoft-core=npm:${pkg.name}@${pkg.version}`);
+  } else {
+    execSync(`npm pkg set devDependencies.${pkg.name}=${pkg.version}`);
+  }
+}
+ 
+execSync("npm install", { stdio: "inherit" });
